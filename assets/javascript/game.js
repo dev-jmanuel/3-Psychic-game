180364b25eb7ae16a66a5354d76d2191d7b42c59
Josh <ejosh.manuel@gmail.com>
2019-01-14 18:26:46 -0800

    asd

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
-//Alphabet array
+// Alphabet array
 var choices = ["a","b","c","d","e","f","g","h","i","j","k","l",
 "m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-//Variables
+// Variables
 var wins = 0;
 var losses = 0;
 var guesses = 10;
@@ -11,8 +11,8 @@ var winsText = document.getElementById("winsText");
 var lossesText = document.getElementById("lossesText");
 var guessesText = document.getElementById("guessesText");
 var lettersText = document.getElementById("lettersText");
-
-//Player functions
+ 
+// Player functions
 document.onkeyup = function(event) {
    
     var userGuess = event.key;
@@ -31,7 +31,7 @@ document.onkeyup = function(event) {
         guesses = 10;
     }
 
-    //Display
+// Display
     winsText.textContent = "Wins: " + wins;
     lossesText.textContent = "Losses: " + losses;
     guessesText.textContent = "Guesses: " + guesses;
